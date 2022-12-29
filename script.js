d63bef11162dd6b090ce9c017a41daaaa659709a
elaine-gomes <109442446+Elaine-Gomes@users.noreply.github.com>
2022-12-29 19:26:51 -0300

    atualização nos nomes das variaveis

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 
-let spanHoras=document.querySelector('#hour');
-let spanMinutos=document.querySelector('#minutes');
-let spanSegundos=document.querySelector('#seconds');
-let horas = document.querySelectorAll(".hours");
+let spanHours=document.querySelector('#hour');
+let spanMinutes=document.querySelector('#minutes');
+let spanSeconds=document.querySelector('#seconds');
+let hours = document.querySelectorAll(".hours");
 let btnTheme = document.querySelector('.themeToggle');
-let valorInputRange = ""
+let valueInputRange = ""
 
 //Atualiza a hora 
-function atualizarHora(){
-    let data = new Date();
-    let hora = corrigirHora(data.getHours());
-    let minutos = corrigirHora(data.getMinutes());
-    let segundos = corrigirHora(data.getSeconds());
+function updateTime(){
+    let date = new Date();
+    let hour= correctHour(date.getHours());
+    let minutes = correctHour(date.getMinutes());
+    let seconds = correctHour(date.getSeconds());
 
-    spanHoras.innerHTML=hora
-    spanMinutos.innerHTML=minutos
-    spanSegundos.innerHTML=segundos
+    spanHours.innerHTML=hour
+    spanMinutes.innerHTML=minutes
+    spanSeconds.innerHTML=seconds
 }
-setInterval(atualizarHora,1000);
+setInterval(updateTime,1000);
 
 //Acrescenta um zero na frente quando o numero é menor do que 10.
-function corrigirHora(numero){
-    if(numero<10){
-        numero='0'+numero
+function correctHour(number){
+    if(number<10){
+        number='0'+number
     }
-    return numero
+    return number
 }
 
 //Modo escuro
 function themeToggle(){
 
     let body = document.querySelector('.body');
-    let relogio = document.querySelector('.clock');
+    let clock = document.querySelector('.clock');
     let onOff = document.querySelector('.onOff');
     let themeToggle = document.querySelector('.themeToggle');
 
     body.classList.toggle('darkTheme');
-    relogio.classList.toggle("darkTheme");
+    clock.classList.toggle("darkTheme");
     themeToggle.classList.toggle("active");
     onOff.classList.toggle("active");
   
-    horas.forEach(item => item.classList.toggle("darkTheme"));
+    hours.forEach(item => item.classList.toggle("darkTheme"));
     
   }
   btnTheme.addEventListener("click", themeToggle);
